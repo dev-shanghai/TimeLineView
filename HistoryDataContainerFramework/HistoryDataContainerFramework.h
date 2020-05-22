becc96552e9f51925b4009073e911c17766afeb5
asim.krews <asim@krews.com>
2020-05-22 16:13:05 +0400

    Initial Commit

---
+++ HistoryDataContainerFramework/HistoryDataContainerFramework.h
@@ -0,0 +1,32 @@
+///**
+
+/**
+HistoryDataContainerFramework
+Created by: Ahmed Alqubaisi on 22/05/2020
+
+HistoryDataContainerFramework
+
+
+
+Copyright (c) 2020   Asim Khan
+
++-----------------------------------------------------+
+|                                                     |
+|                                                     |
+|                                                     |
+|                                                     |
++-----------------------------------------------------+
+
+*/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HistoryDataContainerFramework.
+FOUNDATION_EXPORT double HistoryDataContainerFrameworkVersionNumber;
+
+//! Project version string for HistoryDataContainerFramework.
+FOUNDATION_EXPORT const unsigned char HistoryDataContainerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HistoryDataContainerFramework/PublicHeader.h>
+
+
