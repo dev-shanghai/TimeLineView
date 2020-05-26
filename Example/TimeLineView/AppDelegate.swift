d3014ff9dce9223bb1b8ca611194aa62d84a8d01
iamasimkhanjadoon@gmail.com <asim2125Khan>
2020-05-26 12:13:01 +0400

    Swift version changed from swift 4 to 5.

--- Example/TimeLineView/AppDelegate.swift
+++ Example/TimeLineView/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var months_array = ["December", "November", "October", "September", "August", "July", "June", "May", "April", "March", "Febuary", "January"]
     
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
        
         self.window = UIWindow(frame: UIScreen.main.bounds)
